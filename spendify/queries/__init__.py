ceb5e4da10b9c67e1dc6b03a87e1658b5a87ef98
Tomás Iannello <106415807+iannellotomas@users.noreply.github.com>
2023-11-17 11:44:30 -0300

    Delete spendify directory

--- spendify/queries/__init__.py
+++
@@ -1,1 +0,0 @@
-from .doQuery import doQuery
--- spendify/utilities/__init__.py
+++
@@ -1,3 +0,0 @@
-from .messages import showError, notice
-from .validateDate import isValidDate
-from .validateInputs import validateOptionInput, inputModel
